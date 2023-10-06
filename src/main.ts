347bfca2b52f2d7266869f07702616b0a91d238f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-10-06 17:42:16 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
+import 'dotenv/config';
+import 'reflect-metadata';
+import { serverLogger } from './logger/server-logger/serverLogger';
+import { App } from './app';
+import { RuntimeEnum } from './enums/runtime.enum';
+import { ConsolaInstance } from 'consola';
+import { defaultLogger } from './logger/default-logger';
+import { DataBase } from './db';
+import { dataSource } from './db/data-source';
 
-class Main{
+class Main {
+	constructor(
+		private readonly server: App,
+		private readonly db: DataBase,
+		private readonly logger: ConsolaInstance,
+	) {}
 
-    // constructor(server: )
+	public async bootstrap(): Promise<void> {
+		this.logger.start('Application is starting...');
+		await this.db.init();
+		await this.server.start();
+		this.logger.success('Application is started successfully');
+	}
 }
+
+const server = new App((process.env.MODE as RuntimeEnum) || RuntimeEnum.dev, serverLogger);
+const db = new DataBase(dataSource, defaultLogger);
+const main = new Main(server, db, defaultLogger);
+main.bootstrap();
